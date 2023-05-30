34045fe06abd4ee1e1d791ddf204b7f84093431b
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-30 05:02:48 -0700

    . Copy - Cut - Paste

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3, middle_word, word_last_2 = word[:3], word[1:-1], word[-2:]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
