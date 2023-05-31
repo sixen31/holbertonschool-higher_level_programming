db0312724ace581566aa620e827cd1043e715b13
sixen31 <alexisgrandjean@hotmail.fr>
2023-05-31 06:51:17 -0700

    indentation

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) > 96 and ord(c) < 123:
-        return True
-    else:
-        return False
-print(islower('a'))
+if ord(c) > 96 and ord(c) < 123:
+    return True
+else:
+    return False
