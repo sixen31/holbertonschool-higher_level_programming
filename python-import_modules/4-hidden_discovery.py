655a24d703377975fc51e165313460918bb6636e
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-02 05:31:24 -0700

    correct __import__

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 
 import hidden_4
 
-for name in dir(hidden_4):
-    if not name.startswith("__"):
-        print(name)
+if __name__ == '__main__':
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
