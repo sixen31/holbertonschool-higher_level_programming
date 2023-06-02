58abd1623b19a91686a4c5b5c1a730b009c4d141
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-02 02:27:19 -0700

    add __import__

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
+if __name__ == '__main__':
 
-print("This is the name of the script: ", sys.argv[0])
-print("Number of arguments: ", len(sys.argv))
-print("The arguments are: " , str(sys.argv))
+    print("This is the name of the script: ", sys.argv[0])
+    print("Number of arguments: ", len(sys.argv))
+    print("The arguments are: ", str(sys.argv))
