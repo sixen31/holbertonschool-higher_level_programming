605e0b9c7b5102bcd42c29500ef3c7c69b8145e6
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-02 01:42:26 -0700

    checker

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,10 +4,12 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
+
 result_add = add(a, b)
 result_sub = sub(a, b)
 result_mul = mul(a, b)
 result_div = div(a, b)
+
 
 print("{} + {} = {}".format(a, b, result_add))
 print("{} - {} = {}".format(a, b, result_sub))
