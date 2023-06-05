20142ee0a864a546c5919390a5a41d595cdaf025
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-05 05:30:57 -0700

    pycodestyle

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for num in my_list:
-            print("{}".format(num))
-
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+def print_list_integer(my_list=[]):
+    for num in my_list:
+        print("{:d}".format(num))
