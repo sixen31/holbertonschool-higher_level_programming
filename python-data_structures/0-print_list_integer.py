1237b3647f2668a48feb9c741e10d03a781c0d08
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-05 03:59:27 -0700

    chnge te code for check

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     def print_list_integer(my_list=[]):
-        print('\n'.join("{:d}".format(num) for num in my_list))
+        for num in my_list:
+            print("{}".format(num))
 
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
