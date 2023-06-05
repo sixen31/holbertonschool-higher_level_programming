4f532cc9d5bc00968c145a5b8a7a93151da2163b
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-05 03:51:03 -0700

    change the code for checker

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
     def print_list_integer(my_list=[]):
-        for num in my_list:
-            print("{:d}".format(num))
+        print('\n'.join("{:d}".format(num) for num in my_list))
 
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
