ead2b743bef12705504f7158d7329c244ae445d1
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-05 03:44:14 -0700

    add __name__ in code

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+if __name__ == "__main__":
 
-my_list = [1, 2, 3, 4, 5,]
+    def print_list_integer(my_list=[]):
+        for num in my_list:
+            print("{:d}".format(num))
+
+my_list = [1, 2, 3, 4, 5]
 print_list_integer(my_list)
