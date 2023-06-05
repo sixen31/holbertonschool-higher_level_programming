11130313b32fe8af596db96dc6a1550502ca402a
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-05 03:48:56 -0700

    modification indentation

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
 
     def print_list_integer(my_list=[]):
         for num in my_list:
             print("{:d}".format(num))
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
