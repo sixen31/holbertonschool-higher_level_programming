152d26b8c026e158e7e30e28c7bb76bb3b246470
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-07 03:14:18 -0700

    remove main and better def

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
-
-if __name__ == "__main__":
-    numbers = [1, 2, 3, 4, 5]
-    print(numbers[5:0])
+    print(my_list[len(my_list):0])
