8f6b78f8e786ca1bdd6f5fae8cfadbbb7f85a1fd
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-07 03:08:13 -0700

    added new list way

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,9 @@
 #!/usr/python3
+
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[i]))
+
+if __name__ == "__main__":
+    numbers = [1, 2, 3, 4, 5]
+    print(numbers[5:0])
