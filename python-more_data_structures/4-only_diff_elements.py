f91a862ae1bafb5d06a8b5c8b4cea94cb63041ab
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-09 03:45:02 -0700

    Write a function that returns a set of all elements present in only one set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return set_1.intersection(set_2)
+def only_diff_elements(set_1, set_2):
+    diff_set = set_1.symmetric_difference(set_2)
+    return diff_set
