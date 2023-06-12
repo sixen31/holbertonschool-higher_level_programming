f2386878329407bd38f7888509a0bdca595055b5
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-12 06:06:48 -0700

    reglage probleme

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+class Square:
+    """This class defines a square."""
 
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
