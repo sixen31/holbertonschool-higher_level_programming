a998a1d68427532dd7d698f1896a1b2961f8066d
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-12 03:46:22 -0700

    change code for checker

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Class definition"""
+
+
 class Square:
+    """This class defines a square."""
+
     def __init__(self, size):
         self.__size = size
