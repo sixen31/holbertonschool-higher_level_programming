0273278b01fa9efa0598c51055659091932b399e
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-15 01:37:33 -0700

    change code for checker

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,19 +8,12 @@ This module contains a function that adds 2 integers.
 def add_integer(a, b=98):
     """
     This function adds two integers.
-
-    Args:
-    a: integer or float.
-    b: integer or float.
-
-    Returns:
-    The sum of two integers.
-
-    Raises:
-    TypeError: If a or b is not an int or a float.
     """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    try:
+        result = a + b
+        return int(result)
+    except TypeError:
+        if type(a) is not int:
+            raise TypeError("a must be an integer")
+        else:
+            raise TypeError("b must be an integer")
