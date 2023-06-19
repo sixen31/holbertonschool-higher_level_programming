39776b5c45bba2c94d8fe41359571939d8cd9ddc
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-19 08:37:44 -0700

    add comment & indentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""MyList Class that inherits from List
+"""
+
 
 def is_same_class(obj, a_class):
     """Return True if obj is exactly an instance of a_class; otherwise False."""
