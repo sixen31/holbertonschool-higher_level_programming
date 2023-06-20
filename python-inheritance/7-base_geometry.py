ef3f3ff52e6bc9c20be5bc1849da0b1c1d2146ab
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-20 03:56:41 -0700

    change for checker

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,16 +7,26 @@ class BaseGeometry:
     """Base geometry class."""
 
     def area(self):
+        """A public instance method that raises an Exception
+        Args: nothing
+        Returns: nothing
+        Raises: Exception
         """
-        Raises an Exception with the message 'area() is not implemented'
-        """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """A public instance method that valides value argument.
+        Args:
+            name (str): Name attached to the values
+            value (int): Value, must be a positive integer
+        Returns: nothing
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less or equal to 0
         """
-        Validates the value as an integer
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+
+    if not isinstance(value, int):
+        raise TypeError("{} must be an integer".format(name))
+    if value <= 0:
+        raise ValueError("{} must be greater than 0".format(name))
