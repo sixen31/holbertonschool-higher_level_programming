8977681508e834808bbbfbaf59223479a8a5decc
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-20 01:48:16 -0700

    indentatiton

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,11 +7,15 @@ class BaseGeometry:
     """Base geometry class."""
 
     def area(self):
-        """Raises an Exception with the message 'area() is not implemented'."""
+        """
+        Raises an Exception with the message 'area() is not implemented'
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates the value as an integer."""
+        """
+        Validates the value as an integer
+        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
