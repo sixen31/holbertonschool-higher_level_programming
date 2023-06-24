108f2dc891f3f1d4a03b5189db83f7f4c3b4ac74
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-24 07:30:16 -0700

    gere git

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -43,4 +43,6 @@ class Square(Rectangle):
         super().__init__(size, size)
 
     def __str__(self):
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
+        return "[Square] {}/{}".format(
+        self._Rectangle__width, self._Rectangle__height
+)
