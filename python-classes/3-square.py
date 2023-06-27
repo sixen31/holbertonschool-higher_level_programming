8706b99aadfdd41a2f8427a45283a7de2696288c
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-27 05:05:18 -0700

    change cyheck

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        """
-        Initializer method that creates a new Square object with a given size.
+"""
+This is the "Square" module.
+This module provides a simple Square class with an initialized size.
+"""
 
-        """
+
+class Square:
+    """A class that defines a Square by size"""
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
-        """
-        Calculates and returns the area of the square.
-
-        """
-        return self.__size ** 2
+        """This is a public instance method"""
+        return self.__size * self.__size
