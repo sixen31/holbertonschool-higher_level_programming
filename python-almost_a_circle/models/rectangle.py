55c772869a73b55cfc0eeb275c03cedb2c0af06f
sixen31 <alexisgrandjean@hotmail.fr>
2023-06-28 04:21:48 -0700

    change for check

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -145,4 +145,4 @@ class Rectangle(Base):
     def display(self):
         """Prints the Rectangle instance with the character '#'"""
         for _ in range(self.height):
-            print("#" * self.width)t
+            print("#" * self.width)
