c98d85a3bc1d6c7cff4b90f99ef97ad5f91512f9
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-25 04:21:02 -0700

    checker

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
--- Write a script that creates a table second_table in the database hbtn_0c_0
 -- Creating the second_table table
+
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 	id INT,
 	name VARCHAR(256),
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 );
 
 -- insert line on the table
+
 INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
