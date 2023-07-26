4c5f2111a08af61cf680e538008887c123cdd40d
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 02:17:13 -0700

    supp bank line

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- This script lists privileges for users user_0d_1 and user_0d_2 on localhost.
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- This script creates the MySQL user user_0d_1 with all privileges and sets the password to user_0d_1_pwd.
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
