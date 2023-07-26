bec031d4e04372ceec1d151eda8808438c490610
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 01:49:05 -0700

    change checker

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,14 @@
--- Creating the second_table table
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-  id INT,
-  name VARCHAR(256),
-  score INT
+-- Create table 'second_table' in db 'hbtn_0c_0'
+-- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
+-- db name will be passed as arg to mysql cmd
+-- Not allowed to use SELECT or SHOW
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
 );
+
+-- Script should create 4 records
 
 INSERT INTO second_table VALUES(1, "John", 10);
 INSERT INTO second_table VALUES(2, "Alex", 3);
