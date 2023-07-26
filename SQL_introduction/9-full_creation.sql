9be6390dfaa631fd110e4c89d3236ebf6af67348
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 01:47:55 -0700

    change for checker

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,17 +1,11 @@
--- Create table 'second_table' in db 'hbtn_0c_0'
--- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
--- db name will be passed as arg to mysql cmd
--- Not allowed to use SELECT or SHOW
+-- Creating the second_table table
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
+  id INT,
+  name VARCHAR(256),
+  score INT
 );
 
--- insert line on the table
-
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+INSERT INTO second_table VALUES(1, "John", 10);
+INSERT INTO second_table VALUES(2, "Alex", 3);
+INSERT INTO second_table VALUES(3, "Bob", 14);
+INSERT INTO second_table VALUES(4, "George", 8);
