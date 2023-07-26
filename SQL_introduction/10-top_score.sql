401dc4ac4bb75960e86ac83a1a77e90a4ee9654e
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 01:50:46 -0700

    change for checker

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
 -- Write a script that lists all records of the table second_table of database
-SELECT score, name
-FROM hbtn_0c_0.second_table
+SELECT score, name FROM second_table
 ORDER BY score DESC;
