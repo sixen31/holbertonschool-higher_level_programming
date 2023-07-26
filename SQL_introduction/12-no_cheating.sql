445973ca98cc7bcce5fbb91229c1f254b9126cf1
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 02:00:28 -0700

    change a correct second and add new line

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,6 @@
 -- This SQL script updates the score of Bob to 10 in the table "second_table."
 -- It identifies Bob using his name field, as the id value is not allowed to be use.
-UPDATE secon_table
+
+UPDATE second_table
 SET score = 10
 WHERE name = 'bob';
