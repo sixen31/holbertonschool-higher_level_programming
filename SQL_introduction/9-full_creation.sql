5aa6dd2ed4e4005a139953869d39380f59eb6521
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 01:45:39 -0700

    change comment

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,7 @@
--- Creating the second_table table
-
+-- Create table 'second_table' in db 'hbtn_0c_0'
+-- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
+-- db name will be passed as arg to mysql cmd
+-- Not allowed to use SELECT or SHOW
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
 	id INT,
 	name VARCHAR(256),
