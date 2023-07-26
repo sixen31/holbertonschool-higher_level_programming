385b49ac79f4b55476e2822902042e9ed2d691ab
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-26 03:17:19 -0700

    change for checker

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,11 @@
--- This script creates the MySQL user user_0d_1 with all privileges and sets the password to user_0d_1_pwd.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Create user 'user_0d_1' with all privileges
+-- Password for user set as 'user_0d_1_pwd'
+-- If user already exists, script should not fail
+-- script that the mysql server user 1
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
