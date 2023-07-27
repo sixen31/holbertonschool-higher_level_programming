8d5eabca3041f8c8163d34090fbc4b7ed7751c10
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-27 05:39:16 -0700

    change code

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,8 @@
--- Utiliser la base de données hbtn_0d_tvshows
-USE hbtn_0d_tvshows;
-
--- Sélectionner les émissions avec leur genre_id en utilisant une jointure LEFT JOIN entre tv_shows et tv_show_genres
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- List all shows in db 'hbtn_0d_tvshows'
+-- Each record show display tv_shows.title, tv_show_genres.genre_id
+-- If show doesn't have genre, display NULL
+-- You can use only one SELECT statement
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
