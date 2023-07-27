ae2b89018f57dd98027efaeb80fab8fcce340784
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-27 05:33:17 -0700

    checker

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
--- Utiliser la base de données hbtn_0d_usa
-USE hbtn_0d_usa;
-
--- Sélectionner les villes de Californie en utilisant une sous-requête pour obtenir le state_id pour California
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
+-- Script that lists all the cities of California registered in the database
+SELECT id, name -- Query to list all the cities from California
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California");
