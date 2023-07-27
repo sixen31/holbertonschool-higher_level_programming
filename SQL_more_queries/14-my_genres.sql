e7ad5dffb5562e963a8db58e954b780ee0f8c1fe
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-27 05:48:57 -0700

    for checker

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,9 @@
 -- Ce script SQL récupère tous les genres de la série "Dexter" à partir de la base de données "hbtn_0d_tvshows".
-SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
+SELECT tv_genres.name AS name
+FROM tv_genres
+JOIN tv_show_genres
+     ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows
+     ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+ORDER BY name;
