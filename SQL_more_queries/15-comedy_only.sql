a5cbd05f04418d2a394459fcb6257b7836ed7ac7
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-28 01:44:19 -0700

    add as title

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Cette requête SQL récupère tous les genres de la série "Dexter" à partir de la base de données "hbtn_0d_tvshows" et les trie par nom de genre
-SELECT tv_shows.title
+SELECT tv_shows.title AS title
 FROM tv_shows
 JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
