852b77171c4e4ff80452b81ffe2d47f3cb18fbf2
sixen31 <alexisgrandjean@hotmail.fr>
2023-07-28 01:33:53 -0700

    Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 14-my_genres.sql)

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,6 @@
 -- Cette requête SQL récupère tous les genres de la série "Dexter" à partir de la base de données "hbtn_0d_tvshows" et les trie par nom de genre
-SELECT tv_genres.name AS name
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY name;
+SELECT tv_shows.title
+FROM tv_shows
+JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
