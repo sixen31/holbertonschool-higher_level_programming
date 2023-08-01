f2b456d59634bb244d891295ffa8b4a111b7b6af
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-01 01:18:54 -0700

    change for checker

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+
+
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Utilisation : {} <nom_utilisateur> <mot_de_passe> <nom_base_de_donnees>".format(sys.argv[0]))
+        sys.exit(1)
+
+    nom_utilisateur = sys.argv[1]
+    mot_de_passe = sys.argv[2]
+    nom_base_de_donnees = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=nom_utilisateur, passwd=mot_de_passe, db=nom_base_de_donnees)
+
+        curseur = db.cursor()
+
+        curseur.execute("SELECT * FROM all_states ORDER BY state_code")
+
+        resultats = curseur.fetchall()
+        for ligne in resultats:
+            print(ligne)
+
+        curseur.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Erreur lors de la connexion à la base de données MySQL : {}".format(e))
+        sys.exit(1)
