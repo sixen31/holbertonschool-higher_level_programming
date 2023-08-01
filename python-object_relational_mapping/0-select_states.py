03f065972652cdc339ff1b82bcee889ec737820d
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-01 02:07:47 -0700

    change check

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+"""List all states from a given db sorted in ascending order by id"""
 
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-    user = sys.argv[1]
-    pasd = sys.argv[2]
-    data = sys.argv[3]
+    user_db = sys.argv[1]
+    passwd_db = sys.argv[2]
+    name_db = sys.argv[3]
 
     db = MySQLdb.connect(
-            host="localhost", port=3306, user=user, passwd=pasd, db=data)
+        host="localhost", user=user_db, passwd=passwd_db, db=name_db, port=3306
+    )
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     results = cur.fetchall()
-    for row in results:
-        print(row)
 
-    cur.close()
+    for element in results:
+        print(element)
+
     db.close()
