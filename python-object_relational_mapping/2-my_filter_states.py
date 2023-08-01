a92358499cf5519ddda413f10c501c8e74aa62c9
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-01 05:28:41 -0700

    fix code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-
-"""lists all states, filtered by user input, database hbtn_0e_0_usa"""
+"""Afficher toutes les valeurs dans la table states de hbtn_0e_0_usa
+où le nom correspond à l'argument donné"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    user_db = sys.argv[1]
-    passwd_db = sys.argv[2]
-    name_db = sys.argv[3]
-    state_name = sys.argv[4]
-
     db = MySQLdb.connect(
         host="localhost",
-        user=user_db,
-        passwd=passwd_db,
-        db=name_db,
-        port=3306
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cursor = db.cursor()
+    cursor.execute(
+        """
+        SELECT *
+        FROM states
+        WHERE states.name LIKE BINARY '{}'
+        ORDER BY states.id
+        """.format(sys.argv[4])
+    )
 
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-    results = cursor.fetchall()
-
-    for element in results:
-        print(element)
-
+    cursor.close()
     db.close()
