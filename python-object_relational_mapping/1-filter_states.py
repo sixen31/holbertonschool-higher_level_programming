6e51aab8e072f1a62d3f75feb5fc8905309fe5c4
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-01 04:26:11 -0700

    fix code& syntaxe

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-
+# List all states with a name starting with uppercase N
+# Username, password, and database names are given as user args
 
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Utilisation : {} <nom_utilisateur> <mot_de_passe> <nom_base_de_donnees>".format(sys.argv[0]))
-        sys.exit(1)
-
     user = sys.argv[1]
     pasd = sys.argv[2]
-    data = sys.argv[3]
+    name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=user, passwd=pasd, db=data
+    db = MySQLdb.connect(
+        host="localhost", user=user, passwd=pasd, db=name, port=3306
+    )
 
-    curseur = db.cursor()
+    cursor = db.cursor()
 
-    curseur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        """
+        SELECT * FROM states
+        WHERE BINARY name
+        LIKE 'N%'
+        ORDER BY states.id ASC
+        """
+        )
 
-    resultats = curseur.fetchall()
-    for ligne in resultats:
-        print(ligne)
+    results = cursor.fetchall()
+    for element in results:
+        print(element)
 
-    curseur.close()
     db.close()
