6186bb523bd5b5c2b5f07b75254e49210c649c74
alexis <124585936+sixen31@users.noreply.github.com>
2023-09-26 12:41:03 +0200

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in range(0, len(str)):
-        if (ord(str[i]) >= 97 and ord(str[i]) < 123):
-            num = 32
-        else:
-            num = 0
-        print("{:c}".format(ord(str[i]) - num), end="")
-    print()
