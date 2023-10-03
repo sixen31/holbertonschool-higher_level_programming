778bdad234f2ad2deace87298000f4a87fdaa64b
alex <6267@holbertonstudents.com>
2023-10-03 10:50:50 -0700

    change for checker

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, col in enumerate(row):
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
             if i != len(row) - 1:
-                print("{:d}".format(col), end=" ")
-            else:
-                print("{:d}".format(col), end="")
+                print(" ", end="")
         print("$")
