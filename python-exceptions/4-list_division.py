480ed7e22147a34cd9d6a041a046d0713a740f4d
alex <6267@holbertonstudents.com>
2023-10-05 04:44:05 -0700

    remplace by

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            print("division par 0")
+            print("division by 0")
             result = 0
         except IndexError:
             print("out of range")
