d4df8de4385cb2a081cb5f9e8015c2a48fea41ce
alex <6267@holbertonstudents.com>
2023-10-06 10:36:55 -0700

    pycodstyle

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 
 def no_c(my_string):
     new_string = ''
@@ -7,6 +6,7 @@ def no_c(my_string):
         if letter != 'c' and letter != 'C':
             new_string += letter
     return new_string
+
 
 if __name__ == "__main__":
     print(no_c("Best School"))
