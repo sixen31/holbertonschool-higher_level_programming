b12f4abebeaca66c54bf81618103a5be49debd68
alex <6267@holbertonstudents.com>
2023-10-06 10:35:00 -0700

    change for checker

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+
 def no_c(my_string):
     new_string = ''
-    for letters in my_string:
-        if letters >= "c" or letters != "C":
-            new_string += letters
+    for letter in my_string:
+        if letter != 'c' and letter != 'C':
+            new_string += letter
     return new_string
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
