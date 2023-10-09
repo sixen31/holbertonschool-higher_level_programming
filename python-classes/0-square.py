85af69534113813f8cbd39082a9a6d3600533eb9
alexis <124585936+sixen31@users.noreply.github.com>
2023-10-09 10:26:02 +0200

    Delete python-classes directory

--- python-classes/0-square.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" class definition """
-
-
-class Square():
-    """ This class defines a square. """
--- python-classes/1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Class definition"""
-
-
-class Square:
-    """This class defines a square."""
-
-    def __init__(self, size):
-        self.__size = size
