a074ab3af6a727bbf7e1f48bdbe4ab8a62b311cb
alex <6267@holbertonstudents.com>
2023-10-10 01:26:45 -0700

    supp exe

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,11 +4,7 @@
 
 
 class Square:
-<<<<<<< HEAD
     """Cette classe définit un carré."""
-=======
-"""This class defines a square."""
->>>>>>> 9480eda110f3df3bfc794f47924ee7a627f24b8e
 
     def __init__(self, size):
         self.__size = size
