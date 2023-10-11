b08089fd3d6a377d925ad2ab4de79a9ffbc219be
alex <6267@holbertonstudents.com>
2023-10-11 04:31:27 -0700

    add comment

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""define class"""
 
 
 def add_integer(a, b=98):
     """
     Adds two integers.
 
-    :param a: The first integer.
-    :param b: The second integer (default is 98).
-    :return: The addition of a and b as an integer.
+    param a The first integer.
+    param b The second integer (default is 98).
+    return The addition of a and b as an integer.
     """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
