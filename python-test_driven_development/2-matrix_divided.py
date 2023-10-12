1793a86b0be08bbb3ffbcb23eef1444dc96ee8e9
alex <6267@holbertonstudents.com>
2023-10-12 07:28:24 -0700

    change for checker

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,6 +2,18 @@
 
 
 def matrix_divided(matrix, div):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number (integer or float)")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
     new_matrix = []
     for row in matrix:
         new_row = []
