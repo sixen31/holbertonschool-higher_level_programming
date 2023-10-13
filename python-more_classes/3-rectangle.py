d501682e3706992d65fa2c55a2a4902a06d6ad90
alex <6267@holbertonstudents.com>
2023-10-13 07:15:31 -0700

    Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" Rectangle Module- For rectangular purposes only """
+
+
+class Rectangle:
+    """ Rectangle class """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """ returns area of rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ returns perimeter of rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return '\n'.join(['#' * self.__width] * self.__height)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+    print(str(my_rectangle))
+    print(repr(my_rectangle))
+
+    print("--")
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle)
+    print(repr(my_rectangle))
