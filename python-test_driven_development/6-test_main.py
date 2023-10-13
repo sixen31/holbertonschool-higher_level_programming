9a42ab228dbd2f09cebfaf032d707afb40a031cf
alex <6267@holbertonstudents.com>
2023-10-13 04:41:40 -0700

    Since the beginning you have been creating “Interactive tests”. For this exercise, you will add Unittests

--- python-test_driven_development/6-test_main.py
+++ python-test_driven_development/6-test_main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 max_integer = import('6-max_integer').max_integer
 
+#Multiples tests for Unittest - 6-max_integer
 print(max_integer([1, 2, 3, 4]))
 print(max_integer([-1, -3, -4, -2]))
 print(max_integer([-1, 3, -4, 2]))
--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,32 +2,31 @@
 """Unittest for max_integer([..])
 """
 import unittest
-max_integer = __import__('6-max_integer').max_integer
+max_integer = import('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Class for unittest with
+    """Class for unittest with :
+    
+Module that test correct input
+Module that test empty list
+Module that test types input
+"""
+def test_area(self):
+    self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
+    self.assertAlmostEqual(max_integer([-1, -2, -4, -3]), -1)
+    self.assertAlmostEqual(max_integer([-1, 3, -4, 2]), 3)
+    self.assertAlmostEqual(max_integer([1, 5.4, 4, -2]), 5.4)
+    self.assertAlmostEqual(max_integer([2]), 2)
+    self.assertAlmostEqual(max_integer([-5]), -5)
+    self.assertAlmostEqual(max_integer([1, 1]), 1)
 
-    Module that tests correct input
-    Module that tests empty list
-    Module that tests input types
-    """
+    def test_empty(self):
+        self.assertIsNone(max_integer(), None)
 
-    def test_correct_input(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([-1, -2, -4, -3]), -1)
-        self.assertEqual(max_integer([-1, 3, -4, 2]), 3)
-        self.assertEqual(max_integer([1, 5.4, 4, -2]), 5.4)
-        self.assertEqual(max_integer([2]), 2)
-        self.assertEqual(max_integer([-5]), -5)
-        self.assertEqual(max_integer([1, 1]), 1)
-
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
-
-    def test_input_types(self):
+    def test_types(self):
         self.assertRaises(TypeError, max_integer, ["str", 2, 3])
         self.assertRaises(TypeError, max_integer, [True, "str", 3])
-        self.assertEqual(max_integer(False), False)
-        self.assertEqual(max_integer([False, True, 3]), 3)
-        self.assertEqual(max_integer("higher ASCII value is : v"), "v")
+        self.assertRaises(TypeError, max_integer, False)
+        self.assertAlmostEqual(max_integer([False, True, 3]), 3)
+        self.assertAlmostEqual(max_integer("higher ASCII value is : v"), "v")
