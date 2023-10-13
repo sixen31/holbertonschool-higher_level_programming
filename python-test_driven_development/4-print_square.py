b31a599210a154a8413310970fd63f6b7468296c
alex <6267@holbertonstudents.com>
2023-10-13 01:10:45 -0700

    Write a function that prints a square with the character

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""define class"""
+
+
+def print_square(size):
+    """Prints a square made of '#' characters."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
