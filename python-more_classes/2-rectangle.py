8bd5fe924b3f3aeeb9bbcc666451e1606ad14bed
alex <6267@holbertonstudents.com>
2023-10-13 06:56:34 -0700

    correct statement

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" Rectangle Module- For rectangular purposes only """
+
 
 class Rectangle:
     """ Rectangle class """
@@ -12,7 +14,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if isinstance(value, int) is False:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -24,26 +26,22 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
     def area(self):
+        """ returns area of rectangle """
         return self.__width * self.__height
 
     def perimeter(self):
+        """ returns perimeter of rectangle """
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle(2, 4)
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-    print("--")
-
-    my_rectangle.width = 10
-    my_rectangle.height = 3
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
