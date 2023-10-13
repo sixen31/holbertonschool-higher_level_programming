b3befc0f25ab11727414f84df7363ce8f5cb0585
alex <6267@holbertonstudents.com>
2023-10-13 04:52:30 -0700

    change name

--- python-test_driven_development/6-test_main.py
+++ python-test_driven_development/6-test_main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-max_integer = import('6-max_integer').max_integer
+import 6-max_integer_module
 
-#Multiples tests for Unittest - 6-max_integer
+max_integer = 6-max_integer_module.max_integer
+
+# Multiples tests for Unittest - 6-max_integer
 print(max_integer([1, 2, 3, 4]))
 print(max_integer([-1, -3, -4, -2]))
 print(max_integer([-1, 3, -4, 2]))
