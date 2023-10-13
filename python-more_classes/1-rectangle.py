0efdd6c201ff9edac14348c6fa8457424c548c82
alex <6267@holbertonstudents.com>
2023-10-13 06:20:09 -0700

    correct statement

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,31 +4,38 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
-    def set_width(self, value):
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.width = value
+        self.__width = value
 
-    def get_width(self):
-        return self.width
+    @property
+    def height(self):
+        return self.__height
 
-    def set_height(self, value):
+    @height.setter
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.height = value
+        self.__height = value
 
-    def get_height(self):
-        return self.height
+# Example usage
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
 
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle.__dict__)
