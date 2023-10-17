ab1a6c93cb1bc02464a8acfc3df603de647b889b
alex <6267@holbertonstudents.com>
2023-10-17 11:39:34 -0700

    change code

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""define python"""
+"""A module with a class BaseGeometry"""
+
 
 class BaseGeometry:
-    """BaseGeometry class"""
-
+    """A class with a no implemented method"""
     def area(self):
+        """define area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """define validator"""
 
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
