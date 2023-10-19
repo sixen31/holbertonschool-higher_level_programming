bbe5e47800dd9527b795f40e60cf23b42059c603
alex <6267@holbertonstudents.com>
2023-10-19 09:02:40 -0700

    Write a script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Define python"""
+
+import json
+
+ 
+
+ 
+
+def save_to_json_file(my_obj, filename):
+
+    """write an object to a text file
+
+    args:
+
+        my_obj: object to manipulate
+
+    filename:
+
+        filename: text file
+
+    return:
+
+        object
+
+    """
+
+ 
+
+    with open(filename, "w", encoding="utf-8") as f:
+
+        written = json.dump(my_obj, f)
+
+    return writte
