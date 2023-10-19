96a8b841068621065f83a362c461a334140736eb
alex <6267@holbertonstudents.com>
2023-10-19 09:50:10 -0700

    Write a function that returns an object (Python data structure) represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+import json
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string.
+
+    Args:
+        my_str (str): The JSON string to be deserialized.
+
+    Returns:
+        object: The Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
