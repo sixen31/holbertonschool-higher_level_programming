ad9381c3d48b34a0d439fa31c99928f44ec4edbe
alex <6267@holbertonstudents.com>
2023-10-19 00:56:43 -0700

    comment code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""define python"""
+
+
 def read_file(filename=""):
+    """Reads a text file and prints its contents to stdout.
+
+    Args:
+        filename (str): The name of the file to be read. Defaults to "".
+
+    Returns:
+        None
+    """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
