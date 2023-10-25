a6680a9c0cb2714a96be43e852a409f2cd6a7ffd
alex <6267@holbertonstudents.com>
2023-10-25 00:16:18 -0700

    add base model

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""MyList Class that inherits from List
-"""
+"""Base Class with JSON serialization and file handling."""
 
+import json
+from models.base import Base
 
 class Base:
-    """
-    Base class for managing ID attribute in all future classes.
-    """
+    """Base class for managing ID attribute and JSON serialization."""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        Initialize a Base object.
+        """Initialize a Base object.
 
         Args:
             id (int, optional): The ID for the object. Defaults to None.
@@ -25,3 +24,48 @@ class Base:
     @classmethod
     def get_nb_objects(cls):
         return cls.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            json_str = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)
+        else:
+            new_instance = cls(1)
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_str = file.read()
+                json_list = cls.from_json_string(json_str)
+                return [cls.create(**d) for d in json_list]
+        except FileNotFoundError:
+            return []
+
+if __name__ == "__main__":
+    pass
+
