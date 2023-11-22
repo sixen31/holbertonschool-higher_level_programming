fb5665cefd916a21438cf122c4c5d1703da46e9d
alex <6267@holbertonstudents.com>
2023-11-22 04:31:26 -0800

    comment

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Create the database if it doesn't exist
+-- script create database hbtn_02 and user_0d_2
 CREATE DATABASE IF NOT EXISTS hbt_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
