60989f81cfeb65ae4d520a67f240cef8a6565e91
alex <6267@holbertonstudents.com>
2023-11-22 04:42:59 -0800

    Write a script that creates the table id_not_null on your MySQL server.

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- Créer la table id_not_null avec deux colonnes : id de type INT avec valeur par défaut 1 et name de type VARCHAR(256)
+-- create table id
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
