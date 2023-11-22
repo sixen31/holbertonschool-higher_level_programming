531ab459d52413c1d010502e32739040ca416cd8
alex <6267@holbertonstudents.com>
2023-11-22 05:01:56 -0800

    Write a script that creates the table unique_id on your MySQL server

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
--- Créer la table unique_id avec deux colonnes : id de type INT avec valeur par défaut 1 et contrainte d'unicité, et name de type VARCHAR(256)
+-- create unique_id
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
