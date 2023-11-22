a38da24b5efb2dcde350909b341b8a5967b6a5a0
alex <6267@holbertonstudents.com>
2023-11-22 04:29:14 -0800

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,4 @@
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbt_0d_2;
-
--- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
