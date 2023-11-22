51bc1ff2783d36d064184c4129763d5fc687d0b3
alex <6267@holbertonstudents.com>
2023-11-22 04:37:05 -0800

    Write a script that creates the table force_name on your MySQL server

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,4 @@
--- Créer la table force_name avec deux colonnes : id de type INT et name de type VARCHAR(256)
--- La contrainte NOT NULL est appliquée sur la colonne name pour s'assurer qu'elle ne peut pas être NULL
+--script create table_name my_sql
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
