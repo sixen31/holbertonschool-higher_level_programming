c2a1da173a7d127956dd31c7fb11bde71005fb9a
alex <6267@holbertonstudents.com>
2023-11-23 04:47:50 -0800

    Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,7 @@
--- Cette requête SQL récupère tous les genres de la série "Dexter" à partir de la base de données "hbtn_0d_tvshows" et les trie par nom de genre
--- Joindre la table "tv_shows" avec la table "tv_genres" en utilisant la clause JOIN
--- La condition de jointure est que la colonne "genre_id" de "tv_shows" doit être égale à la colonne "id" de "tv_genres"
--- Sélectionne le titre des émissions de télévision en utilisant un alias "title"
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
 SELECT tv_shows.title AS title
 FROM tv_shows
-JOIN tv_genres ON tv_shows.genre_id = tv_genres.id
+JOIN tv_genres 
+	ON tv_shows.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
