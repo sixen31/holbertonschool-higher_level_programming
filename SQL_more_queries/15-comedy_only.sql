70b90cb7214cb8eaa4d362ce3f178ec16a465c88
alex <6267@holbertonstudents.com>
2023-11-23 06:57:40 -0800

    Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
 -- script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title AS title
+SELECT tv_shows.title title
 FROM tv_shows
-JOIN tv_genres 
-	ON tv_shows.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name LIKE 'Comedy'
+ORDER BY tv_shows.title ASC;
