0c7225bad6ab183e9fe039288e7d234afb6a2523
alex <6267@holbertonstudents.com>
2024-01-31 07:03:27 -0800

    create and add file 1-script.js

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const redHeader = document.getElementById('red_header');
+    const header = document.querySelector('header');
+
+    redHeader.addEventListener('click', function () {
+        header.style.color = '#FF0000';
+    });
+});
